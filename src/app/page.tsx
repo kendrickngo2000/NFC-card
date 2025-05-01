e0c50dc7f349edf8ff879ec1d8ece0201945f8e2
Kendrick Ngo <kendrickngo2@gmail.com>
2025-05-01 04:49:57 -0700

    bypass safari alert for contact card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
   // if yes
   function handleYesClick() {
     setShowHeart(true);
+    downloadContact(); 
     setTimeout(() => {
       setShowHeart(false);
     }, 4000);
