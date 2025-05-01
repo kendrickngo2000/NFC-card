1f32760d3d256b43d3639456ec14da51616e6345
Kendrick Ngo <kendrickngo2@gmail.com>
2025-04-30 21:16:27 -0700

    encoding VCard contact info workaround

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ import { projects } from "../../data/projects";
 
 // VCARD contact info
 function downloadContact() {
-  const vCard = `BEGIN:VCARD\nVERSION:3.0\nFN:Kendrick Ngo♡\nTEL;TYPE=CELL:+7146220884\nEND:VCARD`;
+  const vCard = `BEGIN:VCARD\nVERSION:3.0\nFN:Kendrick Ngo \\u2661\nTEL;TYPE=CELL:+17146220884\nEND:VCARD`;
   const blob = new Blob([vCard], { type: "text/vcard" });
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
